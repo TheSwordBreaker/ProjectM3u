f342bf896324fdffb995a1619d6bbc7326dd762f
TheSwordBreaker <kunalguptadev007@gmail.com>
2020-06-24 23:50:50 +0530

    database file added

---
+++ m3u.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Jun 24, 2020 at 08:19 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.2.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `m3u`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `playlist`
+--
+
+CREATE TABLE `playlist` (
+  `id` int(4) NOT NULL,
+  `name` varchar(1000) NOT NULL,
+  `url` varchar(2000) NOT NULL,
+  `file` varchar(1000) NOT NULL,
+  `createdby` int(3) NOT NULL,
+  `source` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `playlist`
+--
+
+INSERT INTO `playlist` (`id`, `name`, `url`, `file`, `createdby`, `source`) VALUES
+(14, 'test123', '', 'assets/files/test123-12020-06-24-06-08-02M3u-Playlist.m3u', 1, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `role` int(2) NOT NULL DEFAULT 0,
+  `verified` int(1) NOT NULL DEFAULT 0,
+  `username` varchar(512) NOT NULL,
+  `password` varchar(200) NOT NULL,
+  `email` varchar(512) NOT NULL,
+  `hash` varchar(512) NOT NULL,
+  `createdAt` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updateAt` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `role`, `verified`, `username`, `password`, `email`, `hash`, `createdAt`, `updateAt`) VALUES
+(1, 1, 1, 'admin', 'admin', 'testmail@gmail.com', '', '2020-06-24 07:19:09', '2020-06-24 07:19:09'),
+(5, 0, 1, 'testingUser', '12345', 'sifeyes996@unknmail.com', '74bba22728b6185eec06286af6bec36d', '2020-06-24 14:44:05', '2020-06-24 14:44:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `playlist`
+--
+ALTER TABLE `playlist`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`) USING BTREE,
+  ADD UNIQUE KEY `username` (`username`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `playlist`
+--
+ALTER TABLE `playlist`
+  MODIFY `id` int(4) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
