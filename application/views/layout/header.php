da340eae515285fcb2d9d17af2e58e09257dd7cc
TheSwordBreaker <kunalguptadev007@gmail.com>
2020-06-29 17:40:08 +0530

    hii

--- application/views/layout/header.php
+++ application/views/layout/header.php
@@ -20,7 +20,7 @@
     <script src="<?= base_url('assets/js/jquery-3.5.1.min.js') ?>"></script>
    <?php else: ?>
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" ></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" ></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" ></script>
   
     <?php endif ?>
@@ -149,7 +149,7 @@
     <main class="container">
       <div class="row">
       <div id="loadingDiv" >
-        <img src="<?= base_url('assets/img/loading.gif')?>" alt="">
+        <!-- <img src="<?= base_url('assets/img/loading.gif')?>" alt=""> -->
       </div>
       
     
